c2418624ebab4132335994aa7218e41d39cb732a
lilalr <gurennut@gmail.com>
2024-03-14 17:50:10 +0700

    menambah prosedur input

--- 041_InsertionSort/041_InsertionSort.cpp
+++ 041_InsertionSort/041_InsertionSort.cpp
@@ -4,6 +4,27 @@ using namespace std;
 int arr[9];
 int i, n, temp, j;
 
+void input() {				// procedur untuk input
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array : ";				// Output ke layar
+		cin >> n;			// Input dari pengguna
+		if (n <= 20)		// Jika n kurang dari atau sama dengan 20
+			break;			// Keluar dari loop
+		else {				// Jika n lebih dari 20
+			cout << "\rArray dapat mempunyai maksimal 20 elemen.\n";	// Output ke layar
+		}
+	}
+	cout << endl;								// Output baris kosong
+	cout << "=====================" << endl;	// Output ke layar
+	cout << "Masukan Elemen Array" << endl;		// Output ke layar
+	cout << "=====================" << endl;	// Output ke layar
+
+	for (int i = 0; i < n; i++) {				// Looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": ";	// Output ke layar
+		cin >> arr[i];							// Input dari pengguna
+	}
+}
+
 void insertionSort() {  //prosedur
     int i, temp, j;
 
