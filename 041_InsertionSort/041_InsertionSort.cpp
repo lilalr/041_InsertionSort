a3c5c737fafda2f251beb17acc510218941a99f5
lilalr <gurennut@gmail.com>
2024-03-14 17:47:25 +0700

    membuat prosedur InsertionSort

--- 041_InsertionSort/041_InsertionSort.cpp
+++ 041_InsertionSort/041_InsertionSort.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void insertionSort() {
-    int i, n, temp, j;
-    int arr[9]
+int arr[9];
+int i, n, temp, j;
+
+void insertionSort() {  //prosedur
+    int i, temp, j;
+
+    for (i = 1; i <= n; i++) { //step 1
+
+        temp = arr[i]; //step 2
+
+        j = i - 1; //step 3
+
+        while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr[j + 1] = arr[j];            //step 4 a
+            j--;                            // step 4b
+        }
+        arr[j + 1] = temp;      //step 5
+    }
 }
 
 int main()
 {
-    
+
 }
+
